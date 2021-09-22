ab104c1c38ef483a191fad9cd296e7c4429b0eed
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-09-22 03:37:12 -0400

    setup bash script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+apt install git -y
+apt-get install dia -y
+apt install cmake -y
+apt install g++ -y
+apt install lcov -y
+apt install gcovr -y
+apt-get install libxml2-dev -y 
+apt-get install libsfml-dev -y
+apt-get install libboost-test-dev -y
+apt-get install snap -y
+sudo snap install code --classic
+sudo apt-get install -y git cmake g++ tar xz-utils gzip bzip2 zip unzip dia
+sudo apt-get install -y libpthread-stubs0-dev libsfml-dev libxml2-dev docker.io
+sudo apt-get install -y libmicrohttpd-dev
+apt autoremove -y
+
+
