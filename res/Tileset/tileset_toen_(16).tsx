0915bda251005d85aa944894620973fbe24b7849
Arechnoman <maximefarnoud@hotmail.com>
2021-10-01 04:51:29 -0400

    reorganisation des fichiers ressources et créations des tilesets pour la map

---
+++ res/Tileset/tileset_toen_(16).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="game_map_tileset_toen" tilewidth="16" tileheight="16" tilecount="364" columns="7">
+ <image source="../Toen's Medieval Strategy Sprite (16x16)/Tile-set - Toen's Medieval Strategy (16x16) - v.1.0.png" width="112" height="832"/>
+</tileset>
