f6eff551814ad245f352e8527e8159dbd72c1ae6
ubuntu <maximefarnoud@hotmail.com>
2021-11-10 15:19:21 +0100

    création du tileset pour environnement et bâtiments

---
+++ res/Tileset/Units_tileset_(various).tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Units_tileset" tilewidth="1000" tileheight="1000" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="318" height="512" source="../Import/units/Dwarf_Sprite_Sheet1.2v.png"/>
+ </tile>
+ <tile id="1">
+  <image width="448" height="192" source="../Import/units/FREE Pixel Art Enemies - MoonRoar Asset Series - General Baddies/01_Spritesheet/Bat_32x32.png"/>
+ </tile>
+ <tile id="5">
+  <image width="512" height="512" source="../Import/units/knight_character_pack_v3.0_by_rgsdev/16x16 knight 2 v3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="1000" height="1000" source="../Import/units/Mecha-stone Golem/Mecha-stone Golem 0.1/PNG sheet/Character_sheet.png"/>
+ </tile>
+</tileset>
