fe99d746f3fdbc639526de3a22986d7f4dcf45f5
ubuntu <maximefarnoud@hotmail.com>
2021-11-12 22:30:45 +0100

    test de tileset des unités (32x32)

---
+++ res/Tileset/Golem_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Character_sheet" tilewidth="100" tileheight="100" tilecount="100" columns="10">
+ <image source="../Import/units/Mecha-stone Golem/Mecha-stone Golem 0.1/PNG sheet/Character_sheet.png" width="1000" height="1000"/>
+</tileset>
