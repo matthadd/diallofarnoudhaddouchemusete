9b6a16e345315ae85f1fbef2c70386e42a086284
ubuntu <maximefarnoud@hotmail.com>
2021-11-24 14:13:24 +0100

    mise à jour finale des tilesets utilisées

---
+++ res/Tileset/tsx/Build_Env_Tileset_Blue_(32).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Build_Env_Tileset_Blue_(32)" tilewidth="32" tileheight="32" tilecount="7" columns="7">
+ <image source="../../../../../Downloads/Build_Env_Map_Blue_(32).png" width="224" height="32"/>
+</tileset>
---
+++ res/Tileset/tsx/Cyclops_Tilesets_Red_(32).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Cyclops Sprite Sheet Red" tilewidth="32" tileheight="32" tilecount="150" columns="15">
+ <image source="../../../../Downloads/Cyclops Sprite Sheet Red.png" width="480" height="320"/>
+</tileset>
