4f15ee0a96e036638293a31fe2787645754c1ef3
ubuntu <maximefarnoud@hotmail.com>
2021-11-30 11:56:07 +0100

    mise en attente (commentaire) de l'ia en attendant l'engine

---
+++ src/shared/ai/RandomAI.cpp
@@ -0,0 +1,12 @@
+//#include "RandomAI.h"
+
+/*namespace ai{
+    int RandomAI::GenRand(int randRange){
+        std::random_device dev;
+        std::mt19937 rng(dev());
+        std::uniform_int_distribution<std::mt19937::result_type> dist(0,randRange); //distributionin range [0, value]
+
+        std::cout << dist(rng) << std::endl;
+    }
+}
+*/
