cb50160753bbefa8d6a449fd1eafe3d3858d5f89
matthadd <matthis.haddouche@gmail.com>
2022-01-10 15:55:41 +0100

    working on render

--- src/client/render/Scene.cpp
+++ src/client/render/Scene.cpp
@@ -2,10 +2,12 @@
 #include <unistd.h>
 
 using namespace sf;
-namespace render{
-    Scene::Scene(SceneID id, state::State& state,int windowWidth, int windowHeight, std::string title) : _sceneInfo(state){
-    _id = id;
-    /*sf::RenderWindow _window(sf::VideoMode(windowWidth, windowHeight), title);
+namespace render
+{
+    Scene::Scene(SceneID id, state::State &state, int windowWidth, int windowHeight, std::string title) : _sceneInfo(state)
+    {
+        _id = id;
+        /*sf::RenderWindow _window(sf::VideoMode(windowWidth, windowHeight), title);
     
         // on gère les évènements
         while(_window.isOpen()){
@@ -45,30 +47,29 @@ namespace render{
         _window.display();
     }*/
 
-    void Scene::setSceneInfo(state::State& state){
+    void Scene::setSceneInfo(state::State &state)
+    {
         _sceneInfo = state;
     }
 
-    int Scene::render(int* warriors_arr)
+    int Scene::render(int *warriors_arr)
     {
         sf::RenderWindow window(sf::VideoMode(512, 512), "MLB");
-   
+
         while (window.isOpen())
         {
             sf::Event event;
             while (window.pollEvent(event))
             {
-                if(event.type == sf::Event::Closed)
+                if (event.type == sf::Event::Closed)
                     window.close();
             }
             window.clear();
             for (render::Layer l : _Layers)
-            {              
+            {
                 window.draw(l);
                 window.display();
             }
-
-           
         }
     }
 
@@ -81,35 +82,41 @@ namespace render{
             sf::Event event;
             while (window.pollEvent(event))
             {
-                if(event.type == sf::Event::Closed)
+                if (event.type == sf::Event::Closed)
                     window.close();
             }
-            window.clear();
 
             for (render::Layer l : _Layers)
             {
+                printf("[RENDER] for (render::Layer l : _Layers)\n");
                 l.load();
                 window.draw(l);
                 window.display();
             }
 
             sleep(1); // put macro here for frame rate
+                        window.clear();
 
-            while (!_Layers.empty()) {_Layers.pop_back();printf("tchunk-");}
 
-            int array[32*32] = {0};
-                for(auto elt : _sceneInfo._GImanagers)     
-                {
-                    printf("GIM ID : %d\n", elt.second->getID());
-                    // "res/Tileset/png/Unit_Map_(32).png"
-                    add(render::Layer((int)elt.second->getID(), elt.second->getRes(), sf::Vector2u(32, 32), array, 16, 16));
-                }
+            while (!_Layers.empty())
+            {
+                _Layers.pop_back();
+                printf("[RENDER] _Layers.pop_back();\n");
+            }
+
+            int array[32 * 32] = {0};
+            for (auto element : _sceneInfo._GImanagers)
+            {
+                printf("[RENDER] for (auto element : _sceneInfo._GImanagers);\n");
+                printf("GIM ID : %d\n", element.second->getID());
+                // "res/Tileset/png/Unit_Map_(32).png"
+                add(render::Layer((int)element.second->getID(), element.second->getRes(), sf::Vector2u(32, 32), array, 16, 16));
+            }
         }
     }
 
-
     // int Scene::render(int* warriors_arr) // take input as GIMs
-    // {   
+    // {
     // sf::RenderWindow window(sf::VideoMode(512, 512), "MLB");
     // render::Layer warriors(render::MOVABLE, "res/Tileset/png/Unit_Map_(32).png", sf::Vector2u(32, 32), warriors_arr, 16, 16);
     // if (!warriors.load())
@@ -127,6 +134,6 @@ namespace render{
     //     window.draw(warriors);
     //     window.display();
     // }*/
-        
+
     // }
 }
